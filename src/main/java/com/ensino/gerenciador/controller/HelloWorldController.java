0092c2079730f9792580228873442ecf56a39aef
Pâmela Vieira da Silva <57575373+panhavsilva@users.noreply.github.com>
2024-02-28 18:51:04 -0300

    Initial commit

---
+++ src/main/java/com/ensino/gerenciador/controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.ensino.gerenciador.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HelloWorldController {
+    @GetMapping
+    public String hello() {
+        return "Hello World!";
+    }
+}
