3161963c46c257d217a4f790a16dfd7f26d8c539
Pâmela Vieira da Silva <57575373+panhavsilva@users.noreply.github.com>
2024-02-29 16:56:33 -0300

    :sparkles: feat: adiciona métodos a classe aluno

--- src/main/java/com/ensino/gerenciador/model/Aluno.java
+++ src/main/java/com/ensino/gerenciador/model/Aluno.java
@@ -1,23 +1,77 @@
 package com.ensino.gerenciador.model;
 
 import com.ensino.gerenciador.utils.Data;
+import lombok.Getter;
 import lombok.Setter;
 
 import java.text.ParseException;
-import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
 
+@Getter
 public class Aluno {
+    @Getter private static List<Aluno> alunosCadastrados = new ArrayList<>();
     private static Integer proximoId = 1;
 
     private Integer id;
     @Setter private String nome;
-    @Setter private Date dataNascimento;
+    @Setter private String dataNascimento;
+
+    public static Aluno criar(Aluno aluno) throws Exception {
+        if (validar(aluno)){
+            aluno.id = setId();
+            alunosCadastrados.add(aluno);
+        }
+        return aluno;
+    }
+
+    public static Aluno editar(Integer id, Aluno aluno) throws Exception {
+        if (validar(aluno)) {
+            Aluno alunoCadastrado = buscarPorId(id);
+            alunoCadastrado.setNome(aluno.getNome());
+            alunoCadastrado.setDataNascimento(aluno.getDataNascimento());
+            return alunoCadastrado;
+        }
+        return null;
+    }
+
+    public static Aluno buscarPorId(Integer id) throws Exception {
+        for (Aluno aluno : alunosCadastrados) {
+            if (aluno.getId().equals(id)) {
+                return aluno;
+            }
+        }
+        throw new Exception("Nenhum aluno encontrado com o ID informado.");
+    }
+
+    public static Aluno deletar(Integer id) throws Exception {
+        Aluno aluno = buscarPorId(id);
+        alunosCadastrados.remove(aluno);
+        return aluno;
+    }
 
     private static Integer setId() {
         return proximoId++;
     }
 
-    private Date formatarDataNascimento(String dataString) throws ParseException {
-        return Data.stringToDate(dataString);
+    private static boolean validarDataNascimento(String dataString) throws ParseException {
+        try {
+            Data.stringToDate(dataString);
+            return true;
+        } catch (ParseException e) {
+            return false;
+        }
+    }
+
+    private static boolean validar(Aluno aluno) throws Exception {
+        if (aluno.getNome().isEmpty() || aluno.getNome().length() < 3) {
+            throw new Exception("Nome não pode estar em branco e deve conter no mínimo três caracteres.");
+        }
+        if (aluno.getDataNascimento().isEmpty() || !validarDataNascimento(aluno.getDataNascimento())) {
+            throw new Exception(
+                    "Data de nascimento não pode estar em branco e deve estar no formato: DD/MM/AAAA."
+            );
+        }
+        return true;
     }
 }
